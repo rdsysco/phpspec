fbc3672c0e9a4a7c59ebc16edfc4e3346807a447
Mehrdad Hedayati <mehrdad.hedayati@rdsysco.com>
2017-09-07 14:18:38 +0430

    before mul

--- spec/RDSysCo/Calculator/Commands/DivisionCommandSpec.php
+++ spec/RDSysCo/Calculator/Commands/DivisionCommandSpec.php
@@ -2,14 +2,32 @@
 
 namespace spec\RDSysCo\Calculator\Commands;
 
+use RDSysCo\Calculator\Calculator;
 use RDSysCo\Calculator\Commands\DivisionCommand;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class DivisionCommandSpec extends ObjectBehavior
 {
+    public function let(Calculator $calculator)
+    {
+        $this->beConstructedWith($calculator, 10);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType(DivisionCommand::class);
+    }
+
+    function it_executes(Calculator $calculator) {
+        $this->beConstructedWith($calculator, 5);
+        $calculator->div(5)->shouldBeCalled();
+        $this->execute();
+    }
+
+    function it_reverts(Calculator $calculator) {
+        $this->beConstructedWith($calculator, 5);
+        $calculator->mul(5)->shouldBeCalled();
+        $this->undo();
     }
 }
--- src/RDSysCo/Calculator/Commands/DivisionCommand.php
+++ src/RDSysCo/Calculator/Commands/DivisionCommand.php
@@ -2,6 +2,17 @@
 
 namespace RDSysCo\Calculator\Commands;
 
-class DivisionCommand
+use RDSysCo\Calculator\Command;
+
+class DivisionCommand extends Command
 {
+    public function execute()
+    {
+        $this->calculator->div($this->op);
+    }
+
+    public function undo()
+    {
+        $this->calculator->mul($this->op);
+    }
 }
